b697a56d3599aca9568e99f0c55ccfa6e2f052e6
Sean <sean@vestaridefair.com>
2021-01-08 16:56:02 -0800

    01/08 4:55PM identifying the load more comments buttons

--- redditSean/index.js
+++ redditSean/index.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
-const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/'
-
-
+const url =
+  'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/';
 
 (async function () {
   //first line starts the chrome headless browser until you add the option of {headless: false} within launch!
@@ -9,19 +8,19 @@ const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_
   //now you create a new page or new tab
   const page = await browser.newPage();
   //consider console logging out the page page variable and the browser variable to see what it picks up... run console.log({page})
-  console.log({ browser });
+
   //then you navigate to that page and enter the url
   await page.goto(url);
   //takes a screenshot and saves it in the local directory
   await page.screenshot({ path: 'example.png' });
-  //expand all comment threads
-  
+  //expand all comment threads... the 'page.$' await is the same as 'document.querySelector('.morecomments')' and 'page.$$' is the same as 'document.querySelectorAll('.morecomments')' the '.morecomments' is an example of a class name...
+  const expandButtons = await page.$$('.morecomments');
+  console.log(expandButtons.length);
   //select all comments, scrape text and points
 
   //sort comments by points
 
   //insert into google spreadsheets
-
 
   await browser.close();
 })();
