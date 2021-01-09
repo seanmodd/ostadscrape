882efdd8931a022ead2a6cefdbb69fb8beb9664b
Sean <sean@vestaridefair.com>
2021-01-08 18:22:56 -0800

    01/08 6:22PM done with reddit scraper baby

--- redditSean/sheet.js
+++ redditSean/sheet.js
@@ -3,13 +3,19 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 module.exports = class Sheet {
   constructor() {
     this.doc = new GoogleSpreadsheet(
-      '12NrXh-jsOsAeQHKmoxkEfTxoWYNkQcjHuwk5tzMoIzY'
+      '19ltMHN30QH81UsFG_bqvBT8YHYN8fYQvfWy8oxg_BeM'
     );
   }
 
   async load() {
     await this.doc.useServiceAccountAuth(require('./credentials.json'));
     await this.doc.loadInfo();
+  }
+  //we will create a new sheet for every comment thread that we scrape!
+  //make sure to add headerValues within the async addSheet(title!!!)
+  async addSheet(title, headerValues) {
+    await this.doc.addSheet({ title, headerValues });
+    return this.doc.sheetsByIndex.length - 1;
   }
   async addRows(rows, i) {
     const sheet = this.doc.sheetsByIndex[i];
