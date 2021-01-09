3bb4b31ef42edcdd9b5db6185c1d73762cd88ad0
Sean <sean@vestaridefair.com>
2021-01-08 16:30:14 -0800

    01/08 4:29PM just ran the example screenshot of example.com on headless chrome

--- redditSean/index.js
+++ redditSean/index.js
@@ -1,67 +1,14 @@
 const puppeteer = require('puppeteer');
-const secrets = require('./secrets');
-const Sheet = require('./sheet');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
+(async function () {
+  //first line starts the chrome headless browser
+  const browser = await puppeteer.launch();
+  //now you create a new page or new tab
   const page = await browser.newPage();
-  await page.goto('https://instagram.com');
-
-  await page.waitForSelector('input');
-
-  const inputs = await page.$$('input');
-  await inputs[0].type(secrets.USERNAME);
-  await inputs[1].type(secrets.PASSWORD);
-
-  const logInButton = (await page.$$('button'))[1];
-  logInButton.click();
-
-  await page.waitForNavigation();
-
-  const sheet = new Sheet();
-  await sheet.load();
-
-  const USERNAMES = (await sheet.getRows(0)).map((row) => row.username);
-  console.log({ USERNAMES });
-  const profiles = [];
-  for (let USERNAME of USERNAMES) {
-    await page.goto(`https://instagram.com/${USERNAME}`);
-    await page.waitForSelector('img');
-    const imgSrc = await page.$eval('img', (el) => el.getAttribute('src'));
-    const headerData = await page.$$eval('header li', (els) =>
-      els.map((el) => el.textContent)
-    );
-    const name = await page
-      .$eval('header h1', (el) => el.textContent)
-      .catch((err) => true);
-    const desc = await page
-      .$eval('.-vDIg span', (el) => el.textContent)
-      .catch((err) => true);
-    const link = await page
-      .$eval('.-vDIg a', (el) => el.textContent)
-      .catch((err) => true);
-    const profile = { imgSrc, name, desc, link, username: USERNAME };
-    for (let header of headerData) {
-      const [count, name] = header.split(' ');
-      profile[name] = count;
-    }
-    profiles.push(profile);
-  }
-
-  const oldProfiles = await sheet.getRows(1);
-  for (let oldProfile of oldProfiles) {
-    if (USERNAMES.includes(oldProfile.username)) {
-      await oldProfile.delete();
-    }
-  }
-
-  await sheet.addRows(profiles, 1);
+  //then you navigate to that page and enter the url
+  await page.goto('https://example.com');
+  //takes a screenshot and saves it in the local directory
+  await page.screenshot({ path: 'example.png' });
 
   await browser.close();
 })();
-
-// await page.waitForSelector('article a');
-
-// await (await page.$('article a')).click();
-// await page.waitFor(3000);
-// await (await page.$$('button'))[6].click();
--- redditSean/secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USERNAME: 'seanmodd',
-    PASSWORD: '2thepack',
-}
