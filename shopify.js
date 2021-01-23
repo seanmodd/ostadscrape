d74626629fdf4ba1bcf23d693381a91e31390e54
Sean <sean@vestaridefair.com>
2021-01-23 11:49:52 -0800

    Jan 23 21 11:49AM

--- shopify.js
+++ shopify.js
@@ -7,12 +7,19 @@ const puppeteer = require('puppeteer');
       defaultViewport: null,
     });
     const page = await browser.newPage();
+    page.setUserAgent(
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+    );
 
     await page.goto('https://experts.shopify.com/');
     await page.waitForSelector('.section');
 
-    await page.$$;
-    document.querySelectorAll;
+    const sections = await page.$$('.section');
+
+    for (const section of sections) {
+      const button = await section.$('a.marketing-button');
+      button.click();
+    }
   } catch (e) {
     console.log('our error', e);
   }
