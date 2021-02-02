2d8f15330cfdebf312b9cc9a8a23fdb081b5cabd
Sean <sean@vestaridefair.com>
2021-02-02 10:37:59 -0800

    Feb 2 10:37AM

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const { PythonShell } = require('python-shell');
 const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
-const Sheet = require('./sheet');
 
-//1st async function
-//Using Puppeteer to launch chromium browser!
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -16,17 +13,11 @@ const Sheet = require('./sheet');
   await page.goto('https://senpex.com/admin.php');
   await page.type('#txtLogin', process.env.USERNAME);
   await page.type('#txtPass', process.env.PASSWORD);
-
-  //Still 1st async function continued...
-  //Locating the captcha within the HTML...
   const element = await page.$('img[src="lib/captcha.php?id=loginpage"]');
   const rect = await page.evaluate((ele) => {
     const { top, left, bottom, right } = ele.getBoundingClientRect();
     return { top, left, bottom, right };
   }, element);
-
-  //Still 1st async function continued...
-  //Taking screenshot of captcha to interpret it
   await page.screenshot({
     path: 'captcha.png',
     clip: {
@@ -36,102 +27,47 @@ const Sheet = require('./sheet');
       height: rect.bottom - rect.top,
     },
   });
-
-  //Still 1st async function continued...
-  //The Await Promise function to set in place the callback to solve Captcha
-  //Ask Omid why this 'await solve('captcha.png') is allowed to turn into the 2nd async function below, the one with 'async function solve(captcha)'... how is this even possible? it's confusing... and there seems to be nested Promises? Isn't this just callback hell?
   const res = await solve('captcha.png');
   await page.type('#txtCaptcha', res);
-
   await page.click('#btnLogin');
-
-  await page.goto('https://senpex.com/index.php?module=cms_json_log&mid=25');
-
+  //! Below is where the scraping starts!!!!!!
+  await page.goto('https://senpex.com/index.php?module=clnt_packs&mid=37');
   const max = 1;
   var iteration = 1;
-
   var resData = [];
-
+  //? Below is very CONFUSING... ask Omid!
   while (true) {
     let data = await page.$$eval('#table-3 tr', (rows) => {
       return Array.from(rows, (row) => {
         const cols = row.querySelectorAll('td');
         return Array.from(cols, (col) => {
           if (col.querySelector('textarea')) {
-            return ''; //col.querySelector('textarea').value;
+            return ''; //col.querySelector('textarea').value; //? What is going on in this while loop???
           }
           return col.innerText.trim() ? col.innerText : 'sean-00000000---empty';
         });
       });
     });
-
-    // console.log(JSON.stringify(data));
     resData.push(data);
     //get next page
-
     console.log('log data', iteration);
     console.log(resData);
-
     // console.log(`//a[text()='${iteration}']`);
-    const [element] = await page.$x(`(//a[text()='${iteration}'])[1]`);
-
+    const [element] = await page.$x(`(//a[text()='${iteration}'])[1]`); //? What does this mean???
     // console.log(element);
     const next = await page.evaluateHandle(
       (e) => e.parentNode.nextSibling,
       element
     );
-
     await next.click();
-
     await page.waitForTimeout(2000);
     if (iteration > max) break;
     iteration++;
-    // document.querySelectorAll(".pagination li:not(.active)")
-    //scrap page
-
-    ///add some code to go to next page if it availabe of not break
-    // break;
   }
-
   resData = JSON.stringify(resData);
   fs.writeFileSync('result.json', resData);
-
-  const sheet = new Sheet();
-  await sheet.load();
-  const sheetIndex = await sheet.addSheet(title.slice(0, 99), [
-    'points',
-    'text',
-  ]);
-  sheet.addRows(resData, sheetIndex);
-
-  //   await browser.close();
-  /*
-THIS IS WHERE YOU CLICK ON THE MENU BUTTON AND THEN THE SENDERS BUTTON AND START SCRAPING
-  */
-
-  //Show this to Omid... why isn't it working??
-  // async () => {
-  //   const menuBtn = await page.$x('/html/body/div[1]/div/a');
-  //   menuBtn.click();
-  //   const sendersBtn = await page.$x(
-  //     '/html/body/div[3]/div[1]/div/ul/li[4]/ul/li[3]/a'
-  //   );
-  //   sendersBtn.click();
-  // };
-  // const newurl = 'https://senpex.com/index.php?module=clnt_senders&mid=32';
-  //Here is another attempt:
-  // async function run() {
-  //     const browser = await puppeteer.launch({
-  //     headless: false,
-  //   });
-  //   const page = await browser.newPage();
-  //   const menuBtn = await page.$x('/html/body/div[1]/div/a');
-  //   menuBtn.click();
-  // }
 })();
-
-//2nd async function
-//The file titled './captch_resolver.py' will create a new file titled './captcha.png'
+//! Scraping is done and below is captcha python code!!
 async function solve(captcha) {
   return new Promise((resolve, reject) => {
     var scriptPath = path.join(__dirname, './captcha_resolver.py');
